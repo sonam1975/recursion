d3adbe7718bf5cac19bc1c963dce90b1e4bbac85
sonam1975 <56772925+sonam1975@users.noreply.github.com>
2019-12-10 23:52:29 -0800

    recursion review

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,21 @@
 // };
 
 // But instead we're going to implement it from scratch:
+
 var getElementsByClassName = function(className
-) {
-  // your code here
-};
+  ) {
+    var result = []; // since result should be an array
+
+    var findElements = function(element) { // create a function that takes an element
+      if (element.classList && element.classList.contains(className)) { // if the element has the classList and also the classlist contains the className class
+        result.push(element); //push it in the result
+      }
+      if (element.childNodes) { // if there is presence of childnodes
+        element.childNodes.forEach(function(childnode) {
+          findElements(childnode);
+        })
+      }
+    }
+    findElements(document.body);
+    return result;
+  };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,8 +1,33 @@
 // this is what you would do if you liked things to be easy:
 // var stringifyJSON = JSON.stringify;
-
 // but you don't so you're going to write it from scratch:
-
 var stringifyJSON = function(obj) {
   // your code goes here
+  //check whats passed in (string? object? function?)
+  //
+  if (typeof(obj) === 'string') {
+    return '"' + obj + '"';
+  } if (typeof(obj) === 'number') {
+    return obj.toString();
+  } if (typeof(obj) === 'boolean') {
+    return obj.toString();
+  } if (obj === null) {
+    return 'null';
+  } if (typeof obj === undefined) {
+    return undefined;
+  } if (Array.isArray(obj)) {
+    var result = '';
+    for (var i = 0; i < obj.length; i++) {
+      result += stringifyJSON(obj[i]) + ',';
+    }
+    return '[' + result.slice(0, result.length - 1) + ']';
+  } if (typeof obj === "object") {
+    var objResult = '';
+    for (var keys in obj) {
+      if (stringifyJSON(obj[keys])) {
+        objResult += stringifyJSON(keys) + ':' + stringifyJSON(obj[keys]) + ',';
+      }
+    }
+    return '{' + objResult.slice(0, objResult.length - 1) + '}';
+  }
 };
